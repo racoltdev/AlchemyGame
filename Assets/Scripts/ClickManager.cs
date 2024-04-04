f4c347745d38365a5b17f2d4f1312780f7ab3897
JJChan-dev <jeidavignon@gmail.com>
2024-04-04 13:29:00 -0400

    fixed clickManager

--- Assets/Scripts/ClickManager.cs
+++ Assets/Scripts/ClickManager.cs
@@ -3,64 +3,14 @@ using UnityEngine;
 public class ClickManager : MonoBehaviour
 {
     public PlayerMovement playerMovement;
-    public InventoryManager inventoryManager;
 
-    /*public void ClickReaction(InteractData interact)
+    public void ClickReaction(InteractData interact)
     {
         playerMovement.SetTargetPosition(interact.goToPoint.position);
         GetItem(interact); // Pass the interact argument here
-    }*/
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                Item item = hit.collider.GetComponent<Item>();
-                if (item != null)
-                {
-                    GetItem(item);
-                }
-                else
-                {
-                    playerMovement.SetTargetPosition(hit.point);
-                }
-            }
-        }
     }
 
-   
-    private void GetItem(Item item)
-    {
-        /*if (IsValidCraftingItem(item.itemID))
-        {
-
-            inventoryManager.AddItem(item.itemID, item.itemName, item.quantity, item.sprite);
-            Debug.Log("Success! " + item.itemName + "added to inventory.");
-        }
-        else
-        {
-            Debug.Log("Failure: Incorrect itemID!");
-        }
-        Destroy(item.gameObject);*/
-    }
-
-
-    // add crafting code-------------------------------------------------------------
-    //private bool IsValidCraftingItem(int itemID)
-    //{
-   // add crafting code*/
-        
-   // }
-
-
-  //IGNORE BELOW BUT DONT DELETE-----------------------------------------------------     
-        
-        /*(InteractData interact)
+    private void GetItem(InteractData interact)
     {
         // Check if player reached the goToPoint position
         if (Vector2.Distance(playerMovement.transform.position, interact.goToPoint.position) < 1.5f)
@@ -74,6 +24,6 @@ public class ClickManager : MonoBehaviour
             {
                 Debug.Log("Failure: Incorrect itemID!");
             }
-        }*/
-    //}
+        }
+    }
 }
