3a1ca85155e3caac5b8587ec0e09e770cd63fbd5
CoffeeCoded <105815440+CoffeeCoded@users.noreply.github.com>
2024-04-28 22:11:24 -0400

    Next level function working

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -9,4 +9,15 @@ public class NextLevel : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // Call NextLvl function when collision with player occurs
+            NextLvl();
+        }
+    }
 }
+
+
